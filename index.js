f200ee2ed0a4d65c2bed5bb6d25970d9fca91dc7
LucasSVA <lucas.svanadze@gmail.com>
2022-06-21 11:28:55 +0200

    first ecercise finished

---
+++ index.js
@@ -0,0 +1,34 @@
+// --- ex-1
+
+const test = 143
+const bis = 213
+
+
+// if (test === bis) {
+//     console.log("this is false");
+// }
+
+// if (test <= bis) {
+//     console.log("this is true");
+// }
+
+// if (bis > test) {
+
+//     console.log("this is true");
+// }
+
+// if (test < bis) {
+//     console.log("this is true");
+// }
+
+// if (test !==143) {
+//     console.log("this is false");
+    
+// }
+
+// if (test != bis) {
+//     console.log("this is true");
+    
+// }
+
+
